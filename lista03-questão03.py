65a618d5c5a91da602851ada05f2d59a41a12777
Lucas Sales <ldss@ic.ufal.br>
2020-12-19 11:15:39 -0300

    :hammer:

--- lista03-questão03.py
+++ lista03-questão03.py
@@ -1,24 +1,7 @@
 import numpy as np
 import cv2
 
-def show_vectors(vec, string):
-    print(len(vec))
-    s = ""
-    print("VETOR DE "+string)
-    for v in vec:
-        s += "[ "
-        #print("[", end="")
-        for i in v:
-            s += str(i) + " "
-            #print(i, end="")
-            #print(" ", end="")
-        s += "]\n"
-        #print("]")
-    s += "\n"
-    #print(s)
-    return s
-
-
+#Função apenas para gerar uma string do ultimo item do vector
 def stringando(vec):
     s = "[ "
     for v in vec[-1]:
@@ -26,15 +9,14 @@ def stringando(vec):
     s += "]"
     return s
 
-
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# pega os frame
+# Objeto para capturar os frames da webcam
 cap = cv2.VideoCapture(0)
+# Objetos para salvar o video
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 4.0, (640, 480))
-out2 = cv2.VideoWriter('output2.avi', fourcc, 4.0, (640, 480))
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
@@ -44,59 +26,36 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-
-#640.0 w
-#480.0 h
-i = 0
 while(True):
-    # Capture frame-by-frame
+    # Capturando os frames
     ret, frame = cap.read()
-
-    #if ret:
-    #    out.write(frame)
-
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    #ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
-    ret, corners = cv2.findChessboardCorners(frame, (7,6),None)
-
-    #print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    #print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    #print(cap.get(cv2.CAP_PROP_FPS))
-
+    # Detectando o tabuleiro
+    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
     rs, ts = "", ""
     if ret == True:
         objpoints.append(objp)
-
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         imgpoints.append(corners2)
-
         # Draw and display the corners
-        gray = cv2.drawChessboardCorners(gray, (7,6), corners2,ret)
         frame = cv2.drawChessboardCorners(frame, (7,6), corners2,ret)
-
-
+        # Obtendo os vetores de calibração
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-        #vec = show_vectors(rvecs, "ROTAÇÃO")
-        #show_vectors(tvecs, "TRANSLAÇÃO")
         rs, ts = stringando(rvecs), stringando(tvecs)
     
-    # Display the resulting frame
-    out.write(frame)
+    # Escrita na tela dos dados
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame, "ROTATION: "+rs, (10,450), font, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-    cv2.putText(frame, "TRANSLATION: "+ts, (10,470), font, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-    cv2.imshow('frame',frame)
-    out2.write(frame)
-    i += 1
+    cv2.putText(frame, "ROTATION: " + rs, (10,450), font, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(frame, "TRANSLATION: " + ts, (10,470), font, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+    # Exibindo na tela
+    cv2.imshow('Lista 03 - Questão 03',frame)
+    # Salvando o video com os vetores em tela
+    out.write(frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 out.release()
-out2.release()
 cv2.destroyAllWindows()
-
-
-#https://docs.opencv.org/3.1.0/d6/d6e/group__imgproc__draw.html#ga5126f47f883d730f633d74f07456c576
